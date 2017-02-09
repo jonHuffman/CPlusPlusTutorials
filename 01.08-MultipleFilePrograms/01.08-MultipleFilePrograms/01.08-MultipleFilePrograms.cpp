cf8c83d53a255ef368b7f35eed58d2d2dac60d96
Jon Huffman <jon@jonhuffman.com>
2017-02-09 10:08:55 -0500

    Completed chapter 1.8

---
+++ 01.08-MultipleFilePrograms/01.08-MultipleFilePrograms/01.08-MultipleFilePrograms.cpp
@@ -0,0 +1,15 @@
+// 01.08-MultipleFilePrograms.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int add(int x, int y);
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << std::endl;
+
+    return 0;
+}
+
---
+++ 01.08-MultipleFilePrograms/01.08-MultipleFilePrograms/add.cpp
@@ -0,0 +1,6 @@
+#include "stdafx.h"
+
+int add(int x, int y)
+{
+    return x + y;
+}
